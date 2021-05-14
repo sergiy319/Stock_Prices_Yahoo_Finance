9db5b48e2b87c054b50914fb8715ee9483dc55eb
Sergiy Naumov <1414sergiy@gmail.com>
2021-05-14 16:16:31 +0300

    Use bs4 for parsing

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Create custom headers.
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
+                  'AppleWebKit/537.36 (KHTML, like Gecko)'
+                  'Chrome/90.0.4430.212 Safari/537.36'
+}
 
+# Save in a variable the url of a specific page
+# from which we are going to parse information.
+urls = ['https://finance.yahoo.com/quote/PD/history?p=PD',
+        'https://finance.yahoo.com/quote/ZUO/history?p=ZUO',
+        'https://finance.yahoo.com/quote/PINS/history?p=PINS',
+        'https://finance.yahoo.com/quote/ZM/history?p=ZM',
+        'https://finance.yahoo.com/quote/DOCU/history?p=DOCU',
+        'https://finance.yahoo.com/quote/CLDR/history?p=CLDR',
+        'https://finance.yahoo.com/quote/RUN/history?p=RUN']
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Save the method for obtaining information into a variable.
+r = requests.get(urls)
 
+# Create a variable for parsing.
+soup = BeautifulSoup(r.text, 'html.parser')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+price = soup.find('span', {'class': 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+change = soup.find('span', {'class': 'C($primaryColor) Fz(24px) Fw(b)'}).text
+
+print(price, change)
