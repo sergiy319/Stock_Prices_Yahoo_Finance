3ae5ac17b39d606bd6c23edae0e4ca407afd4cc2
Sergiy Naumov <1414sergiy@gmail.com>
2021-05-15 10:35:33 +0300

    Create a parsing function

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-# Create custom headers.
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
-                  'AppleWebKit/537.36 (KHTML, like Gecko)'
-                  'Chrome/90.0.4430.212 Safari/537.36'
-}
+# Put a list of company names into a variable
+list_company_names = [
+    'PD', 'ZUO', 'PINS', 'ZM',
+    'PVTL', 'DOCU', 'CLDR', 'RUN']
 
-# Save in a variable the url of a specific page
-# from which we are going to parse information.
-urls = ['https://finance.yahoo.com/quote/PD/history?p=PD',
-        'https://finance.yahoo.com/quote/ZUO/history?p=ZUO',
-        'https://finance.yahoo.com/quote/PINS/history?p=PINS',
-        'https://finance.yahoo.com/quote/ZM/history?p=ZM',
-        'https://finance.yahoo.com/quote/DOCU/history?p=DOCU',
-        'https://finance.yahoo.com/quote/CLDR/history?p=CLDR',
-        'https://finance.yahoo.com/quote/RUN/history?p=RUN']
+# Create an empty list for adding data
+download_data = []
 
-# Save the method for obtaining information into a variable.
-r = requests.get(urls)
 
-# Create a variable for parsing.
-soup = BeautifulSoup(r.text, 'html.parser')
+# Create a parsing function
+def get_financial_data(company_name):
+    # Create custom headers.
+    my_headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
+                      'AppleWebKit/537.36 (KHTML, like Gecko)'
+                      'Chrome/90.0.4430.212 Safari/537.36'}
 
-price = soup.find('span', {'class': 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
+    # Save in a variable the url of a specific page
+    # from which we are going to parse information.
+    url_parsing_page = f'https://finance.yahoo.com/quote/{company_name}/history?p={company_name}'
 
-change = soup.find('span', {'class': 'C($primaryColor) Fz(24px) Fw(b)'}).text
+    # Save the method for obtaining information into a variable.
+    receiving_information = requests.get(url_parsing_page)
 
-print(price, change)
+    # Create a variable for parsing result.
+    parsing_result = BeautifulSoup(receiving_information.text, 'html.parser')
+
+    # Create a variable for data for the whole period.
+    stock_data = {
+        'company_name': company_name,
+        'data_whole_period': parsing_result.find('...').find_all('...').text
+    }
+
+    return stock_data
+
+
+# We create a loop to process the data of all the necessary company names.
+for name in list_company_names:
+    download_data.append(get_financial_data(name))
+
+# Create a file to save data to Json.
+with open('download_data.json', 'w') as dj:
+    json.dump(download_data, dj)
